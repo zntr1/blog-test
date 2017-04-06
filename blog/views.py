a59dd7cb84b6dc3a93fc6ba522bfc17482c1fbce
zntr1 <zesarex@gmail.com>
2017-04-06 17:10:31 +0200

    variablen mit template gemacht, filter geändert

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,6 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    postliste = Post.objects.all().order_by('published_date')
+	# 'posts ist die var im template, postliste die im view
+    return render(request, 'blog/post_list.html', {'posts': postliste})
