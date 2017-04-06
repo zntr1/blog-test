d5243535b1f2eb94c7b1d8ff328cc6a989c6662e
zntr1 <zesarex@gmail.com>
2017-04-06 17:02:58 +0200

    variablen mit template gemacht

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
